cf249c431e450e15cc75b02937dda566b8c404cb
rayjaylc <tacomatrailhunter66@gmail.com>
2026-03-24 01:49:19 +0000

    Update workspace.js

--- script/workspace.js
+++ script/workspace.js
@@ -40,5 +40,5 @@ trottleBtn.addEventListener('click',()=>{
 })
 
 requestMeeting.addEventListener('click',()=>{
-  window.location.href = "/rayjayllc/consulting/contact.html#meeting"
+  window.location.href = "/rayjallc/consulting/contact.html#meeting"
 })
